208e1d6d0e887ec6e14f6455cf4022a3c1ca8e94
Tanya Ribakova <tanya2021rib@gmail.com>
2023-01-22 12:46:52 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+    SalesManager Manager1 = new SalesManager(new int[]{150, 200, 30,50,450});
+    SalesManager Manager2 = new SalesManager (new int[]{50, 500, 20,100,380});
+    SalesManager Manager3 = new SalesManager (new int[]{250, 20, 150,300,15});
+    System.out.println("Продажи (в тыс.руб.)");
+    System.out.println ("Менеджер 1 : " + Manager1.max());
+    System.out.println ("Менеджер 2 : " + Manager2.max());
+    System.out.println ("Менеджер 3 : " + Manager3.max());
+
+    }
+}
